0f42790bfd3d3e4298c9dd5644b3ebf668bf7c4b
Aleksey Timofeev <atimofeev1508@gmail.com>
2025-08-26 02:52:40 +0300

    Create web server with client to LLM

---
+++ src/main/java/com/alextim/myai/controller/StreamingChatController.java
@@ -0,0 +1,22 @@
+package com.alextim.myai.controller;
+
+import com.alextim.myai.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+public class StreamingChatController {
+
+    private final ChatService chatService;
+
+    @GetMapping(value = "/chat-stream/{chatId}",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter talkToModel(@PathVariable Long chatId, @RequestParam String userPrompt){
+        return chatService.proceedInteractionWithStreaming(chatId,userPrompt);
+    }
+}
